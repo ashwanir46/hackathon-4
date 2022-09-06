56507ee37a0bdf5f6b03803a4b4f1f288ff5bae2
Ashwani Rajput <ashwanir461@gmail.com>
2022-09-06 09:42:35 +0530

    All attribute done

--- src/main/java/com/jap/ticketing/Hello.java
+++ src/main/java/com/jap/ticketing/Hello.java
@@ -1,8 +1,20 @@
 package com.jap.ticketing;
 
 public class Hello {
+    private String schedule_no;
+    private String route_no;
+    private int ticket_from_stop_id;
+    private int ticket_from_stop_seq_no;
+    private int ticket_till_stop_id;
+    private int ticket_till_stop_seq_no;
+    private String ticket_date;
+    private String ticket_time;
+    private int total_ticket_amount;
+    private double travelled_KM;
+
 
     public static void main(String[] args) {
+
         System.out.println("Hello");
     }
 }
